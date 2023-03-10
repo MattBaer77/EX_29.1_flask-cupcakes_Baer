f07195bf9d8cab20671f24ba3248391f7e28faa9
Matthew Baer <matt.baer77@gmail.com>
2023-03-09 23:30:37 -0500

    added cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, render_template, flash, redirect
+from sneakybeaky import SECRET_GEORGE
+# from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = SECRET_GEORGE
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
